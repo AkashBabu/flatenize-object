53143e0d8f3d6ed7b5145c045762c12eae67b68b
Akash Babu <001akashbabu@gmail.com>
2016-10-09 00:00:51 +0530

    Major bug fixes and added Documentation

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
+
+
+
+
+
+
+
+
+
 # flatenize-object
+
+A Nodejs Library to flattenize an object within another object
+
+## Installation
+>   npm install flattenize-object --save
+
+## Usage
+
+```javascript
+var flattenize = require('flattenize-object');
+var obj = {
+  "a": 1,
+  "b": {
+    "c": [
+      2,
+      3,
+      4,
+      5
+    ],
+    "d": 1,
+    "e": "string",
+    "inObj": {
+      "f": 123,
+      "g": [
+        1,
+        2,
+        3
+      ]
+    }
+  }
+}
+```
+
+console.log(JSON.stringify(flatten(obj, 'b.inObj')));
+```javascript
+{
+  "a": 1,
+  "b": {
+    "c": [
+      2,
+      3,
+      4,
+      5
+    ],
+    "d": 1,
+    "e": "string",
+    "f": 123,
+    "g": [
+      1,
+      2,
+      3
+    ]
+  }
+}
+
+
+```
+
+console.log(JSON.stringify(flatten(obj, 'b'), undefined, 2));
+```javascript
+{
+  "a": 1,
+  "c": [
+    2,
+    3,
+    4,
+    5
+  ],
+  "d": 1,
+  "e": "string",
+  "f": 123,
+  "g": [
+    1,
+    2,
+    3
+  ]
+}
+
+
+```
+
--- index.js
+++ index.js
@@ -1,52 +1,39 @@
-
-
-
-
-
-
-
-
-
-
-
-
 function validKeyPath(keyPath) {
     var keys = keyPath.split('.');
-    keys.forEach( key => {
-        if(!key)
-            return false;   
+    keys.forEach(key => {
+        if (!key)
+            return false;
     })
     return true;
 }
 
-function getObj(obj, key){
-	console.assert(obj[key], 'Given param doesnt exist');
+function getObj(obj, key) {
+    console.assert(obj[key], 'Given param doesnt exist');
     console.assert(obj[key].constructor == Object, 'Can only unwind an Array inside an Object');
-	return obj[key];
+    return obj[key];
 }
 
-function getInnerObj(obj, keyPath){
-	var keys = keyPath.split('.');
-	var tempObj = obj;
-	for(var i = 0; i < (keys.length); i++){
-        tempObj = getObj(tempObj, keys[i]);
-	}
-	return tempObj;
+function getInnerObj(obj, keyPath) {
+    var keys = keyPath.split('.');
+    var tempObj = obj;
+    keys.forEach(key => {
+        tempObj = getObj(tempObj, key);
+    })
+    return tempObj;
 }
 
-module.exports = function(obj, keyPath){
-   console.assert(obj.constructor == Object, 'Only Objects are allowed to flattenize');
-//    console.assert(validKeyPath(keyPath), 'Wrong KeyPath');
-   var keys = keyPath.split('.');
-   var flattened = JSON.parse(JSON.stringify(obj));
-   var inObj = getInnerObj(flattened, keyPath);
-   var inPreObj = getInnerObj(flattened, keys.slice(0, -1).join('.'));
-   Object.assign(inPreObj, inObj);
-   delete inPreObj[keys.slice(-1)[0]];
-   return flattened;
-   
-//    inPreObj[keys[-1]]
-//    Object.assign(flattened, flattened[keyPath]);
-//    delete flattened[keyPath];
-//    return flattened;
+module.exports = function(obj, keyPath) {
+    console.assert(obj.constructor == Object, 'Only Objects are allowed to flattenize');
+    var keys = keyPath.split('.');
+    var flattened = Object.assign({}, obj);
+    var inObj = getInnerObj(flattened, keyPath);
+    if (keys.length > 1) {
+        var inPreObj = getInnerObj(flattened, keys.slice(0, -1).join('.'));
+        Object.assign(inPreObj, inObj);
+        delete inPreObj[keys.slice(-1)[0]];
+    } else {
+        flattened = Object.assign(flattened, inObj);
+        delete flattened[keyPath];
+    }
+    return flattened;
 }
